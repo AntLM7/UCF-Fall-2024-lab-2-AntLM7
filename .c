3579db55c6c48b06d894a53bbc7cdd1ad799a566
AntLM7 <an379171@ucf.edu>
2024-09-12 23:09:31 -0400

    Update .c

--- .c
+++ .c
@@ -2,31 +2,39 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Function to generate a random number between 1 and 10
+// generate a random number between 1 and 10
 int generate_random_number() {
-    return (rand() % 10) + 1;  // Generates a number between 1 and 10
+    return (rand() % 10) + 1;  // Using modulo to keep the number within the range of 1 to 10
 }
 
-// Function to play the guessing game
-void play_game() {
-    int random_number = generate_random_number();
+// user input and validation, making sure the input is within the correct range
+int get_guess() {
     int guess;
-    int attempts = 3;  // The player has 3 attempts
+    while (1) {
+        printf("Enter your guess: ");
+        if (scanf("%d", &guess) == 1 && guess >= 1 && guess <= 10) {
+            return guess;  // If the input is valid, we return it
+        } else {
+            printf("Invalid input. Please enter a number between 1 and 10.\n");
+            // Clear the buffer in case the user enters something invalid
+            while (getchar() != '\n');
+        }
+    }
+}
+
+// main function that runs the game logic
+void play_game() {
+    int random_number = generate_random_number();  // Generate the random number
+    int guess;
+    int attempts = 3;  // The user gets 3 attempts
 
     printf("Guess the number (between 1 and 10). You have 3 attempts.\n");
 
+    // Using a loop to give the user multiple chances
     for (int i = 0; i < attempts; i++) {
-        printf("Enter your guess: ");
-        
-        // Check for correct input
-        if (scanf("%d", &guess) != 1) {
-            printf("Invalid input. Please enter a number.\n");
-            // Clear the input buffer in case of an invalid input
-            while (getchar() != '\n');
-            i--;  // Don't count invalid input as an attempt
-            continue;
-        }
+        guess = get_guess();  // Call the function to get the user's guess
 
+        // use conditional statements to check if the guess is correct, too high, or too low
         if (guess == random_number) {
             printf("Congratulations! You guessed the number.\n");
             return;
@@ -35,18 +43,21 @@ void play_game() {
         } else {
             printf("Too low! Try again.\n");
         }
+
+        // let user know how many attempts they have left after each wrong guess
+        if (i < attempts - 1) {
+            printf("You have %d attempt(s) left.\n", attempts - i - 1);
+        }
     }
 
+    // If the user runs out of attempts, reveal the correct number
     printf("Sorry, you've used all your attempts. The number was: %d\n", random_number);
 }
 
 int main() {
-    // Seed the random number generator
-    srand(time(0));
+    srand(time(0));  // Set the random number generator to ensure we get a different number each time
 
-    // Start the game
-    play_game();
+    play_game();  // Start the game by calling the play_game function
 
     return 0;
 }
-
